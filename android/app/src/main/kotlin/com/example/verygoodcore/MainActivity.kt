123b2649e811fb5cc00b4b9ed769c4f726a40146
Srujan <srujanmhase5@gmail.com>
2022-03-29 19:21:10 +0530

    init: new flutter project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.beapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/view/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatefulWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  _HomePageViewState createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
